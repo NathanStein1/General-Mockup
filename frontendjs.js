79687cc23f8d803c4793d17eee33b79a63a8dd9f
NathanStein1 <nathanstein2@gmail.com>
2022-04-30 11:48:10 -0700

    test2

---
+++ frontendjs.js
@@ -0,0 +1,10 @@
+var loader = document.querySelector('.loader')
+
+var hide = document.querySelector('.loaderBtn')
+var show = document.querySelector('.loaderBtn2')
+
+hide.addEventListener('click', function hide(){
+    loader.classList.add('hide')
+
+})
+
