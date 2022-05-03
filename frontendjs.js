838000ee726080bb749720425b0a5d50a80f1c0e
NathanStein1 <nathanstein2@gmail.com>
2022-05-02 20:17:20 -0700

    particles

--- frontendjs.js
+++ frontendjs.js
@@ -1,6 +1,6 @@
 var loader = document.querySelector('.loader')
 var hide = document.querySelector('.loaderBtn')
-var searchFunction=document.querySelector('.searchFunction')
+var searchFunction = document.querySelector('.searchFunction')
 
 
 var dropBtn = document.querySelector('.firstBtn')
@@ -18,10 +18,10 @@ var pad2 = document.querySelector('.pad2')
 
 var corpimg1 = document.querySelector('.corpimg1')
 
-hide.addEventListener('click', function hide(){
-    
+hide.addEventListener('click', function hide() {
 
-    if (loader.classList.contains('hide')){
+
+    if (loader.classList.contains('hide')) {
         loader.classList.remove('hide')
 
     }
@@ -34,63 +34,79 @@ hide.addEventListener('click', function hide(){
 
 
 
-dropAmazon1.addEventListener('click', function changeNameAma(){
+dropAmazon1.addEventListener('click', function changeNameAma() {
     dropBtn.textContent = "Amazon"
     colorChange1();
-    
+
 })
 
-dropWalmart1.addEventListener('click', function changeNameWal(){
-    dropBtn.textContent ="Walmart"
+dropWalmart1.addEventListener('click', function changeNameWal() {
+    dropBtn.textContent = "Walmart"
     colorChange1();
 })
 
-dropAmazon2.addEventListener('click', function changeNameAma1(){
+dropAmazon2.addEventListener('click', function changeNameAma1() {
     dropBtn1.textContent = "Amazon"
     colorChange2();
-    
+
 })
 
-dropWalmart2.addEventListener('click', function changeNameWal1(){
-    dropBtn1.textContent ="Walmart"
+dropWalmart2.addEventListener('click', function changeNameWal1() {
+    dropBtn1.textContent = "Walmart"
     colorChange2();
 })
 
 
 
-function colorChange1(){
-    if (dropBtn.textContent == "Amazon"){
+function colorChange1() {
+    if (dropBtn.textContent == "Amazon") {
         pad1.style.backgroundImage = "url(amazon.jpg)";
         pad1.style.backgroundSize = "contain"
         pad1.style.backgroundRepeat = "no-repeat"
-        pad1.style.color ="white"
+        pad1.style.color = "white"
     }
-    else if (dropBtn.textContent == "Walmart"){
-        
+    else if (dropBtn.textContent == "Walmart") {
+
         pad1.style.backgroundImage = "url(walmart.jpg)";
         pad1.style.backgroundSize = "contain"
         pad1.style.backgroundRepeat = "no-repeat"
-        pad1.style.color ="white"
+        pad1.style.color = "white"
     }
 }
 
-function colorChange2(){
-    if (dropBtn1.textContent == "Amazon"){
+function colorChange2() {
+    if (dropBtn1.textContent == "Amazon") {
         pad2.style.backgroundImage = "url(amazon.jpg)";
         pad2.style.backgroundSize = "contain"
         pad2.style.backgroundRepeat = "no-repeat"
-        pad2.style.color ="white"
+        pad2.style.color = "white"
     }
-    else if (dropBtn1.textContent == "Walmart"){
+    else if (dropBtn1.textContent == "Walmart") {
         pad2.style.backgroundImage = "url(walmart.jpg)";
         pad2.style.backgroundSize = "contain"
         pad2.style.backgroundRepeat = "no-repeat"
-        pad2.style.color ="white"
+        pad2.style.color = "white"
     }
 }
 
+window.
+    onload
+    = function () {
+        Particles.
+            init
+            ({
+                selector:
+                    '.background',
+                color: '#E9E9FF',
+                connectParticles:
+                    true,
+                maxParticles: 150,
+                
 
 
+
+            });
+    };
 
 
 
